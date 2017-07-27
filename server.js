1427ea360b3060c4f66e07b0dd8e10e468da3669
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2017-07-27 09:26:42 -0500

    added files

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict'; 
+var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var PORT = process.env.PORT || 8085;
+var app = express(); 
+
+app.use(bodyParser.json());
+app.get('/', function (req, res) {
+    res.send('index.html')
+}) 
+
+app.listen(PORT, function () {
+    console.log('listening on port ' + PORT) 
+  });

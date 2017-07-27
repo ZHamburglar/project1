4190be8521b496c5a83d69f575c76fdb3f55af31
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2017-07-27 10:05:20 -0500

    uploading code

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 $(document).ready(function(){
   $('#getSomething').on('click',getJPic)
+  $('#inputField').on('change', searchByName)
 });
+
+
 
   function getJPic(){
     var request = $.ajax({
             url: 'https://app3-gse00012260.apaas.us2.oraclecloud.com/api/getallrefinance',
             type: 'GET',
             dataType: 'json'
-        });
+          });
     request.done(function(response){
       console.log(response[0]);
       for (i = 0; i < response.length; i++) {
-        var rowId = response[i].id
+        var rowId = i+1
         var originationMortgagee = response[i].companyName
         var propertyType = response[i].propertyType
         var totalResult = response[i].amount
